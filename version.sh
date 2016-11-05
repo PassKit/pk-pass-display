3f183801f27efa564e000eae08005b246cc659e9
Duncan Brown <duncan@passkit.com>
2016-11-06 00:01:24 +0800

    added echo messages to pre version script

--- version.sh
+++ version.sh
@@ -11,11 +11,15 @@ if [ $branch_name != "master" ]; then
 fi
 
 #switch branch to distribution branch and merge latest changes from master
+echo "merging master branch into dist branch"
 git checkout dist
 git merge master -q --no-edit --log --no-commit
 #build the distribution files and then finish merge
+echo "building dist files"
 gulp
+echo "adding any changed dist files to commit"
 git add -A
 if ! git diff-index --quiet HEAD --; then
     git commit --no-edit
 fi
+echo "preversion script finished"
